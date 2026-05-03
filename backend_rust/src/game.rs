abcbbf92fc865a78eb9b3a93381107c0c2b20703
Lars Janssen <lars.janssen@live.nl>
2026-05-03 14:56:22 +0000

    feat(rust backend): game logic

--- backend_rust/src/game.rs
+++ backend_rust/src/game.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::fmt;
 
 #[derive(PartialEq, Eq, Hash, Debug)]
 pub enum Action {
@@ -10,24 +11,33 @@ pub enum Action {
 
 static DIE: &[u8] = &[1, 2, 3, 4, 5, 6];
 const N_DICE: u8 = 8;
+const WORM: u8 = 6;
+
+#[derive(Debug)]
+pub struct GameStateCreationError;
+impl fmt::Display for GameStateCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Number of dice in game must me smaller than {N_DICE}")
+    }
+}
+
 
 pub struct GameState {
     hand: Vec<u8>,
     dice_throw: Option<Vec<u8>>,
 }
 impl GameState {
-    pub fn new(hand: Vec<u8>, dice_throw: Option<Vec<u8>>) -> Self {
-        GameState { hand, dice_throw }
+    pub fn new(hand: Vec<u8>, dice_throw: Option<Vec<u8>>) -> Result<GameState, GameStateCreationError> {
+        let valid_size = hand.len() + dice_throw.as_ref().map(|v| v.len()).unwrap_or(0) <= N_DICE.into();
+        match valid_size {
+            true => {
+                Ok(GameState { hand, dice_throw })
+            }
+            false => Err(GameStateCreationError),
+        }
     }
-}
 
-pub trait State {
-    fn available_actions(&self) -> Vec<Action>;
-    fn compute_score(&self) -> u32;
-}
-
-impl State for GameState {
-    fn available_actions(&self) -> Vec<Action> {
+    pub fn available_actions(&self) -> Vec<Action> {
         if self.hand.len() >= N_DICE as usize {
             return vec![Action::Stop];
         }
@@ -49,13 +59,24 @@ impl State for GameState {
         }
     }
 
-    fn compute_score(&self) -> u32 {
-        let mut score = 0;
-        if self.hand.contains(&6) {
-            for die in &self.hand {
-                score += *die as u32;
-            }
+    pub fn compute_score(&self) -> u8 {
+        if !self.hand.contains(&WORM) {
+            return 0;
         }
-        score
+        self.hand.iter().map(|&die| {
+            match die {
+                6 => 5,
+                val => val,
+            }
+        }).sum()
     }
 }
+
+// pub fn expected_score(state: GameState, action: Action) -> u8 {
+//     match action {
+//         Action::Bust => 0,
+//         Action::Stop => state.compute_score(),
+//         Action::SaveDice(d) =>
+
+//     }
+// }
--- backend_rust/src/lib.rs
+++ backend_rust/src/lib.rs
@@ -1,24 +1,63 @@
 mod game;
-mod tree;
 
-struct ValuedAction;
 
-pub fn run(hand: Vec<u8>, dice_throw: Option<Vec<u8>>, c: f64) -> Vec<ValuedAction> {
-    let actions = crate::tree::run(hand, dice_throw, c);
-    vec![ValuedAction]
-}
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn compute_no_worm() {
+        let state = game::GameState::new(
+            vec![5, 5], None
+        ).unwrap();
+        assert_eq!(state.compute_score(), 0)
+    }
+
+    #[test]
+    fn compute_with_worm() {
+        let state = game::GameState::new(
+            vec![5, 6], None
+        ).unwrap();
+        assert_eq!(state.compute_score(), 10)
+    }
+
+    #[test]
+    fn stop_at_full_hand() {
+        let state = game::GameState::new(
+            vec![5, 5 , 5 ,5,5,5,5,5], None
+        ).unwrap();
+        let state_actions = state.available_actions();
+        assert_eq!(state_actions, [game::Action::Stop]);
+    }
+
+    #[test]
+    fn bust_at_no_option() {
+        let state = game::GameState::new(
+            vec![5, 5 , 5 ,5,5,5], Some(vec![5, 5])
+        ).unwrap();
+        let state_actions = state.available_actions();
+        assert_eq!(state_actions, [game::Action::Bust]);
+    }
+
+    #[test]
+    fn test_save_dice_options() {
+        let state = game::GameState::new(
+            vec![5, 5 , 5 ,5,5,5], Some(vec![5, 6])
+        ).unwrap();
+        let state_actions = state.available_actions();
+        assert_eq!(state_actions, [game::Action::SaveDice(6)]);
+    }
+
+    #[test]
+    fn test_roll_or_stop() {
+        let state = game::GameState::new(
+            vec![5, 5 , 5 ,5,5,5], None
+        ).unwrap();
+        let state_actions = state.available_actions();
+        assert!(
+            state_actions.contains(&game::Action::Roll) & state_actions.contains(&game::Action::Stop)
+        )
     }
 }
