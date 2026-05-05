5853476ff6ca6f18f64497a42cdf588ed13ffb4d
Lars Janssen <lars.janssen@live.nl>
2026-05-05 20:24:08 +0000

    fix(rust): add distinct roll outcomes computation

--- backend_rust/src/game.rs
+++ backend_rust/src/game.rs
@@ -1,5 +1,8 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
+use std::collections::binary_heap::Iter;
 use std::fmt;
+use itertools::Itertools;
+use itertools::structs::CombinationsWithReplacement;
 
 #[derive(PartialEq, Eq, Hash, Debug)]
 pub enum Action {
@@ -10,6 +13,7 @@ pub enum Action {
 }
 
 static DIE: &[u8] = &[1, 2, 3, 4, 5, 6];
+const N_FACES: u8 = 6;
 const N_DICE: u8 = 8;
 const WORM: u8 = 6;
 
@@ -19,6 +23,11 @@ impl fmt::Display for GameStateCreationError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Number of dice in game must me smaller than {N_DICE}")
     }
+}
+
+struct ProbGameState {
+    prob: f64,
+    state: GameState,
 }
 
 
@@ -72,11 +81,106 @@ impl GameState {
     }
 }
 
-// pub fn expected_score(state: GameState, action: Action) -> u8 {
-//     match action {
-//         Action::Bust => 0,
-//         Action::Stop => state.compute_score(),
-//         Action::SaveDice(d) =>
+pub fn max_expected_score(state: GameState, action: Action) -> u8 {
+    match action {
+        Action::Bust => 0,
+        Action::Stop => state.compute_score(),
+        Action::SaveDice(select_die) => {
+            match state.dice_throw {
+                Some(dice_throw) => {
+                    let mut new_hand = state.hand.clone();
 
-//     }
-// }
+                    let saved_dice = dice_throw
+                        .iter()
+                        .filter(|&&die| die == select_die).copied().collect();
+
+                    new_hand.extend(saved_dice);
+                    let new_state = GameState::new(state.hand, None).unwrap();
+
+                    let max_score = new_state.available_actions().iter().map(
+                        |&action| max_expected_score(new_state, action)
+                    ).max().unwrap();
+
+                    max_score
+                },
+                _ => panic!("No dice thrown, while selecting die is not allowed"),
+            }
+
+        },
+        Action::Roll => {
+            let outcomes_iter = create_distinct_roll_outcomes_iter(state)
+            // TODO
+            // Compute weighted average over max_expected_score of ProbStates
+            1
+        }
+
+    }
+}
+
+
+fn create_distinct_roll_outcomes_iter(state: GameState) -> Iter<ProbGameState> {
+    let throw_count = (N_DICE as usize) - state.hand.len();
+    let n_outcomes = (N_FACES as f64).powi(N_DICE as i32);
+
+    // Calculate weight using multinomial coefficient: n! / (n1! * n2! * ... * nk!)
+    // where n is total dice, and ni is count of each face
+    let combination_iter = DIE.iter().combinations_with_replacement(throw_count)
+    .map(move |outcome | {
+            let prob = (
+                compute_relative_probability(&outcome)
+            ) / (n_outcomes as f64);
+
+            let owned_outcome: Vec<u8> = outcome.into_iter().cloned().collect();
+
+            let new_game_state = GameState::new(
+                state.hand.clone(),
+                Some(owned_outcome)
+            ).unwrap();
+
+            ProbGameState{
+                prob,
+                state: new_game_state,
+            }
+
+        })
+
+}
+
+fn compute_relative_probability(outcome: &Vec<&u8>) -> f64 {
+    let counts = counter(outcome);
+    let num_dice = outcome.len();
+
+    let rel_prob = (factorial(num_dice) as f64) / (
+        counts.iter().map(
+            |&count| factorial(count as usize) as f64
+        ).product::<f64>()
+    );
+
+    rel_prob
+
+}
+
+fn factorial(integer: usize) -> usize {
+    if integer < 2 {
+        return 1
+    }
+    integer * factorial(integer - 1)
+}
+
+fn counter(input: &[&u8]) -> Vec<u8> {
+    let mut counts = HashMap::new();
+    let mut order = Vec::new();
+
+    for &&num in input {
+        let count = counts.entry(num).or_insert(0u8);
+        if *count == 0 {
+            order.push(num);
+        }
+        *count += 1;
+    }
+
+    order.into_iter()
+        .map(|num| counts[&num])
+        .collect()
+
+}
